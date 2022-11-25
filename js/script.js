af395ef54eeaa683812729981055ef6b007831b4
Shoddior <shoxshoxziz262@gmail.com>
2022-11-25 20:47:06 +0500

    script

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 
-// const time=document.querySelector(".js-conteiner" );
-// const next=document.querySelector(".js-work")  ;
-// const clear=document.querySelector(".js-works")  ;
+const time=document.querySelector(".js-conteiner" );
+const next=document.querySelector(".js-work")  ;
+const clear=document.querySelector(".js-works")  ;
 
-// let counter=0;
+let counter=0;
 
-// time.innerText=counter
+time.innerText=counter
 
-// next.addEventListener("click",()=>{
-// counter+=1;
-// time.innerText=counter
-// });
-// clear.addEventListener("click",()=>{
-//     counter=0
-//     time.innerText=counter
+next.addEventListener("click",()=>{
+counter+=1;
+time.innerText=counter
+});
+clear.addEventListener("click",()=>{
+    counter=0
+    time.innerText=counter
     
-// })
+})
 
 
-// console.log(counter);
+console.log(counter);
 
 
 
